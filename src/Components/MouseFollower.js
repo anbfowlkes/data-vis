b643bd65395669740d057e80d0e0bd05bc5b640c
Aaron Fowlkes <anbfowlkes@gmail.com>
2022-09-23 15:42:23 -0400

    first

---
+++ src/Components/MouseFollower.js
@@ -0,0 +1,30 @@
+import React, { useState, useCallback } from 'react';
+
+
+
+const MouseFollower = () => {
+
+  const width = 960;
+  const height = 500;
+  const circleRadius = 30;
+  const initialMousePosition = { x: width / 2, y: height / 2 };
+
+  const [mousePosition, setMousePosition] = useState(initialMousePosition);
+  
+  const handleMouseMove = useCallback(event => {
+    const { clientX, clientY } = event;
+    setMousePosition({ x: clientX, y: clientY });
+  }, [setMousePosition]);
+  
+  return (
+    <svg width={width} height={height} onMouseMove={handleMouseMove}>
+      <circle
+        cx={mousePosition.x - 70}
+        cy={mousePosition.y}
+        r={circleRadius}
+      />
+    </svg>
+  );
+};
+
+export default MouseFollower
